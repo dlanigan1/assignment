bada3ff09c564828a58bb7091b89f5eff290bd58
David <dlanigan@tssg.org>
2018-03-13 10:11:20 +0000

    added title to books page

--- src/books.js
+++ src/books.js
@@ -14,12 +14,16 @@ class Books extends React.Component {
         let books = api.getAll() ;
 
         return (
-          <BootstrapTable data={books} striped={true} hover={true} cellEdit={ cellEditProp }>
-              <TableHeaderColumn dataField="id" isKey={true} dataAlign="center" dataSort={true}>ID</TableHeaderColumn>
-              <TableHeaderColumn dataField="title" dataAlign="center" dataSort={true}>Name</TableHeaderColumn>
-              <TableHeaderColumn dataField="author" dataAlign="center" dataSort={true}>Author</TableHeaderColumn>
-              <TableHeaderColumn dataField="status" dataAlign="center" dataSort={true}>Status</TableHeaderColumn>
-          </BootstrapTable>
+          <div>
+            <h1>List of Books ({books.length})</h1>
+            <p>double click on a cell to edit</p>
+            <BootstrapTable data={books} striped={true} hover={true} cellEdit={ cellEditProp }>
+                <TableHeaderColumn dataField="id" isKey={true} dataAlign="center" dataSort={true}>ID</TableHeaderColumn>
+                <TableHeaderColumn dataField="title" dataAlign="center" dataSort={true}>Name</TableHeaderColumn>
+                <TableHeaderColumn dataField="author" dataAlign="center" dataSort={true}>Author</TableHeaderColumn>
+                <TableHeaderColumn dataField="status" dataAlign="center" dataSort={true}>Status</TableHeaderColumn>
+            </BootstrapTable>
+          </div>
         );
 
     }
