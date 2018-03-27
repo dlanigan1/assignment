6632e77ff5dba4afff3403c5bd00dcdc53e3b6d1
David Lanigan <dlanigan@tssg.org>
2018-03-27 22:07:56 +0100

    removed redundant api methods

--- src/test/stubAPI.js
+++ src/test/stubAPI.js
@@ -11,13 +11,6 @@ class StubAPI {
         this.books = books;
     }
 
-    delete(k) {
-        let elements = _.remove(this.books,
-            (book) => book.title === k
-        );
-        return elements;
-    }
-
     getAllBooks() {
         return this.books ;
     }
@@ -39,24 +32,6 @@ class StubAPI {
       }
       return "" ;
 
-    }
-    add(n,a,p) {
-        let len = this.books.length ;
-        let newLen = this.books.push({
-            title: n, author : a, available: p }) ;
-        return newLen > len ;
-    }
-
-    update(key,n,a,p) {
-        let index = _.findIndex(this.books,
-            (book) => book.title === key
-        );
-        if (index !== -1) {
-            this.books.splice(index, 1,
-                {title: n, author: a, available: p});
-            return true ;
-        }
-        return false ;
     }
 }
 
